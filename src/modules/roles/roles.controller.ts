dc5624dd6ccbff11c171fc2b124c3a5c6ca19b61
Giper8ola <matwey.peregudow2100@gmail.com>
2024-02-14 21:41:02 +0300

    14.02.2024

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -1,13 +1,4 @@
-import {
-    Controller,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-    Request,
-    UseGuards
-} from '@nestjs/common';
+import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
